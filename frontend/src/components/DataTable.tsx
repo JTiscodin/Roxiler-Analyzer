3ddd8bb42ce0694ecf62bc076ea01662c79ac448
Jatin Tilwani <jatintilwani500@gmail.com>
2024-06-21 15:53:02 +0530

    type fixes for deployment

--- frontend/src/components/DataTable.tsx
+++ frontend/src/components/DataTable.tsx
@@ -18,58 +18,47 @@ type TableProps = {
 };
 
 export const DataTable = ({ data }: TableProps) => {
-  let headings;
-  if (data?.response.length > 0) headings = Object.keys(data?.response[0]);
+  const headings: (keyof Item)[] = data?.response.length ? Object.keys(data.response[0]) as (keyof Item)[] : [];
 
   return (
-    <ScrollArea className="w-[80vw] h-[85vh] border-2 overflow-hidden  border-orange-600 rounded-2xl">
+    <ScrollArea className="w-[80vw] h-[85vh] border-2 overflow-hidden border-orange-600 rounded-2xl">
       <Table>
         <TableCaption>List of all the products.</TableCaption>
-        {data?.response.length !== 0 ? (
+        {data?.response.length ? (
           <>
             <TableHeader>
               <TableRow>
-                {headings
-                  ? headings.map((e) => {
-                      return (
-                        <TableHead key={e} className="w-[100px]">
-                          {e}
-                        </TableHead>
-                      );
-                    })
-                  : ""}
+                {headings.map((heading) => (
+                  <TableHead key={heading} className="w-[100px]">
+                    {String(heading)}
+                  </TableHead>
+                ))}
               </TableRow>
             </TableHeader>
             <TableBody>
-              {data ? (
-                data.response.map((e) => {
-                  return (
-                    <TableRow key={e.id}>
-                      {headings.map((heading) => (
-                        <TableCell key={heading} className="font-medium">
-                          {heading === "image" ? (
-                            <img src={e[heading]} />
-                          ) : heading === "sold" ? (
-                            e[heading] ? (
-                              "Yes"
-                            ) : (
-                              "No"
-                            )
-                          ) : (
-                            e[heading]
-                          )}
-                        </TableCell>
-                      ))}
-                    </TableRow>
-                  );
-                })
-              ) : (
-                <TableRow>Loading</TableRow>
-              )}
+              {data.response.map((item) => (
+                <TableRow key={item.id}>
+                  {headings.map((heading) => (
+                    <TableCell key={heading} className="font-medium">
+                      {heading === "image" ? (
+                        <img src={item[heading] as string} alt={`Product ${item.id}`} />
+                      ) : heading === "sold" ? (
+                        item[heading] ? "Yes" : "No"
+                      ) : (
+                        String(item[heading])
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))}
             </TableBody>
           </>
         ) : (
-          <TableRow className="text-center font-extrabold text-4xl">No Content Found</TableRow>
+          <TableRow>
+            <TableCell colSpan={headings.length} className="text-center font-extrabold text-4xl">
+              {data?.response.length === 0 ? "No Content Found" : "Loading"}
+            </TableCell>
+          </TableRow>
         )}
       </Table>
     </ScrollArea>
